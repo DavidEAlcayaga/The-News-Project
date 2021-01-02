742b2f51ce895b5bcc4b83bf174cd0b0d6b93b4f
tigerflight <david.canto@alumnos.ucn.cl>
2021-01-02 20:21:02 -0300

    feat(Model): add eloquent parameters for news model

--- web/app/Models/News.php
+++ web/app/Models/News.php
@@ -7,5 +7,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class News extends Model
 {
+
+    /**
+     * Indicates if the model's ID is auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * The database connection that should be used by the model.
+     *
+     * @var string
+     */
+    protected $connection = 'sqlite';
+
     use HasFactory;
 }
