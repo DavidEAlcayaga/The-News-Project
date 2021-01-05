ef43cef54180951f1b69f7fc82f9ca3ea1aabaaf
Ricardo Ortiz Hidalgo <72056246+ricardoOrtizUCN@users.noreply.github.com>
2021-01-05 00:40:53 -0300

    fix(web): Fix update web

--- web/routes/web.php
+++ web/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\NewsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('registernews');
 });
+
+Route::view('registernews', 'registernews');
+Route::post('registernews', [NewsController::class, 'store']);
