bbea6e7b8754f11bdce92013fa36cbc6febea353
Ricardo Ortiz Hidalgo <72056246+ricardoOrtizUCN@users.noreply.github.com>
2021-01-05 00:38:44 -0300

    feat(NewsController): Add data reception NewsController

--- web/app/Http/Controllers/NewsController.php
+++ web/app/Http/Controllers/NewsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\News;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 class NewsController extends Controller
 {
@@ -19,7 +20,7 @@ class NewsController extends Controller
 
         // Return the get Request
         return response([
-            'message'=>'Retrieved Succesfully','news'=>$news
+            'message' => 'Retrieved Succesfully', 'news' => $news
         ], 200);
     }
 
@@ -36,18 +37,38 @@ class NewsController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+        $this->validate($request, [
+            'title' => 'required',
+            'author' => 'required',
+            'url' => 'required',
+            'description' => 'required',
+            'content' => 'required',
+            'date' => 'required'
+        ]);
+
+        $news = new News();
+        $news->title = $request->input('title');
+        $news->author = $request->input('author');
+        $news->url = $request->input('url');
+        $news->url_image = $request->input('url_image');
+        $news->description = $request->input('description');
+        $news->content = $request->input('content');
+        $news->published_at = $request->input('date');
+        $news->save();
+        $news = News::all();
+
+        return back()->with('message', 'You have added a news successfully!');
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
@@ -58,7 +79,7 @@ class NewsController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
@@ -69,8 +90,8 @@ class NewsController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
@@ -81,7 +102,7 @@ class NewsController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
