e5839038b41e927a284d36c8788364be5528c915
tigerflight <david.canto@alumnos.ucn.cl>
2021-01-14 04:57:22 -0300

    feat(Controller): replace show function content to use the NewsResource, and add the index method to get all the news using NewsCollection

--- web/app/Http/Controllers/Api/NewsController.php
+++ web/app/Http/Controllers/Api/NewsController.php
@@ -4,33 +4,23 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\News;
+use App\Http\Resources\NewsResource;
+use App\Http\Resources\NewsCollection;
 use Illuminate\Http\Request;
 
 
 //TODO agregar php docs y definir controllers distintos para api y webpage o un solo controlador
 class NewsController extends Controller
 {
+    public function index()
+    {
+        return NewsCollection::make(
+            News::orderBy('id')->get()
+        );
+    }
+
     public function show(News $news)
     {
-        return response()->json([
-            'data' => [
-                'type' => 'news',
-                'id' => $news->id,
-                'attributes' => [
-                    'id' => (string)$news->id,
-                    'title' => $news->title,
-                    'author' => $news->author,
-                    'source' => $news->source,
-                    'url' => $news->url,
-                    'url_image' => $news->url_image,
-                    'description' => $news->description,
-                    'content' => $news->content,
-                    'published_at' => $news->published_at,
-                ],
-                'links' => [
-                    'self' => url('/api/v1/news/'.$news->getRouteKey())
-                ]
-            ]
-        ]);
+        return NewsResource::make($news);
     }
 }
