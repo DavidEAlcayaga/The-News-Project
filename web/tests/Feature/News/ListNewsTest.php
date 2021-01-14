ae979a02c3188187c7e5332c84ae9a8e68b48234
tigerflight <david.canto@alumnos.ucn.cl>
2021-01-14 04:52:41 -0300

    feat(Test): add test to fetch all news in json format

--- web/tests/Feature/News/ListNewsTest.php
+++ web/tests/Feature/News/ListNewsTest.php
@@ -23,7 +23,7 @@ class ListNewsTest extends TestCase
         $response->assertJson([
             'data' => [
                 'type' => 'news',
-                'id' => $news->id,
+                'id' => (string) $news->id,
                 'attributes' => [
                     'id' => (string)$news->id,
                     'title' => $news->title,
@@ -38,6 +38,77 @@ class ListNewsTest extends TestCase
                 'links' => [
                     'self' => route('api.v1.news.show', $news)
                 ]
+            ]
+        ]);
+    }
+
+    /** @test */
+    public function can_fetch_all_news()
+    {
+
+        $news = News::factory()->times(3)->create();
+
+        $response = $this->getJson(route('api.v1.news.index'));
+
+        $response->assertJson([
+            'data' => [
+                [
+                    'type' => 'news',
+                    'id' => $news[0]->id,
+                    'attributes' => [
+                        'id' => (string)$news[0]->id,
+                        'title' => $news[0]->title,
+                        'author' => $news[0]->author,
+                        'source' => $news[0]->source,
+                        'url' => $news[0]->url,
+                        'url_image' => $news[0]->url_image,
+                        'description' => $news[0]->description,
+                        'content' => $news[0]->content,
+                        'published_at' => $news[0]->published_at,
+                    ],
+                    'links' => [
+                        'self' => route('api.v1.news.show', $news[0])
+                    ]
+                ],
+                [
+                    'type' => 'news',
+                    'id' => $news[1]->id,
+                    'attributes' => [
+                        'id' => (string)$news[1]->id,
+                        'title' => $news[1]->title,
+                        'author' => $news[1]->author,
+                        'source' => $news[1]->source,
+                        'url' => $news[1]->url,
+                        'url_image' => $news[1]->url_image,
+                        'description' => $news[1]->description,
+                        'content' => $news[1]->content,
+                        'published_at' => $news[1]->published_at,
+                    ],
+                    'links' => [
+                        'self' => route('api.v1.news.show', $news[1])
+                    ]
+                ],
+                [
+                    'type' => 'news',
+                    'id' => $news[2]->id,
+                    'attributes' => [
+                        'id' => (string)$news[2]->id,
+                        'title' => $news[2]->title,
+                        'author' => $news[2]->author,
+                        'source' => $news[2]->source,
+                        'url' => $news[2]->url,
+                        'url_image' => $news[2]->url_image,
+                        'description' => $news[2]->description,
+                        'content' => $news[2]->content,
+                        'published_at' => $news[2]->published_at,
+                    ],
+                    'links' => [
+                        'self' => route('api.v1.news.show', $news[2])
+                    ]
+                ],
+            ],
+            'links' => [
+                'self' => route('api.v1.news.index')
             ]
         ]);
     }
