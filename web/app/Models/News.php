9e7b0bbbc14603bea6a2d8ec2409009d12eaf599
tigerflight <david.canto@alumnos.ucn.cl>
2021-01-16 01:03:43 -0300

    feat(Model): add casts to the news attributes and the scope function to apply sorts to a collection

--- web/app/Models/News.php
+++ web/app/Models/News.php
@@ -2,8 +2,11 @@
 
 namespace App\Models;
 
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 //TODO agregar php docs
 class News extends Model
@@ -26,10 +29,37 @@ class News extends Model
      *
      * @var string
      */
-   protected $connection = 'sqlite';
+    protected $connection = 'sqlite';
 
     use HasFactory;
 
-   protected $guarded = ["id"];
+    protected $guarded = ["id"];
+
+    /**
+     * @var array
+     */
+    protected $casts = [
+        'id' => 'integer',
+        'published_at' => 'string',
+    ];
+
+    /**
+     * @param Builder $query
+     * @param $sort
+     * @mixin Eloquent
+     */
+    public function scopeApplySorts(Builder $query, $sort){
+
+        $sortFields = Str::of($sort)->explode(',');
+
+        foreach($sortFields as $sortField){
+            $direction = 'asc';
+            if(Str::of($sortField)->startsWith('-')){
+                $direction = 'desc';
+                $sortField = Str::of($sortField)->substr(1);
+            }
+            $query->orderBy($sortField, $direction);
+        }
+    }
 
 }
