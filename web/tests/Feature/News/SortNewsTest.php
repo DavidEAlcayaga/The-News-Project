fafe63abd06fb7ad67aaa26e247a14df8f2d3c1f
tigerflight <david.canto@alumnos.ucn.cl>
2021-01-16 00:58:02 -0300

    feat(Test): add it_can_sort_news_by_last_date to test the ascendant and descendant order for the list of news

--- web/tests/Feature/News/SortNewsTest.php
+++ web/tests/Feature/News/SortNewsTest.php
@@ -11,18 +11,32 @@ class SortNewsTest extends TestCase
     use RefreshDatabase;
 
     /** @test */
-    public function it_can_sort_news_by_recent_date()
+    public function it_can_sort_news_by_last_date()
     {
-        News::factory()->create(['published_at' => '09:00:00']);
-        News::factory()->create(['published_at' => '10:00:00']);
-        News::factory()->create(['published_at' => '08:00:00']);
+        News::factory()->create(['published_at' => '2019-12-02T20:01:00.283041Z']);
+        News::factory()->create(['published_at' => '2020-12-02T20:01:00.283041Z']);
+        News::factory()->create(['published_at' => '2018-12-02T20:01:00.283041Z']);
 
         $url = route('api.v1.news.index', ['sort' => 'published_at']);
         $this->getJson($url)->assertSeeInOrder([
-            '10:00:00',
-            '09:00:00',
-            '08:00:00',
+            '2018-12-02T20:01:00.283041Z',
+            '2019-12-02T20:01:00.283041Z',
+            '2020-12-02T20:01:00.283041Z',
         ]);
+    }
 
+    /** @test */
+    public function it_can_sort_news_by_recent_date()
+    {
+        News::factory()->create(['published_at' => '2019-12-02T20:01:00.283041Z']);
+        News::factory()->create(['published_at' => '2020-12-02T20:01:00.283041Z']);
+        News::factory()->create(['published_at' => '2018-12-02T20:01:00.283041Z']);
+
+        $url = route('api.v1.news.index', ['sort' => '-published_at']);
+        $this->getJson($url)->assertSeeInOrder([
+            '2020-12-02T20:01:00.283041Z',
+            '2019-12-02T20:01:00.283041Z',
+            '2018-12-02T20:01:00.283041Z',
+        ]);
     }
 }
