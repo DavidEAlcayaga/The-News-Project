15441f8b4241f65c0831ed5f8fa3dad4fa1835c8
Pablo-Castillo <pablo.castillo01@alumnos.ucn.cl>
2021-01-18 05:29:28 -0300

    feat(services): add AppDatabase class to handle local DB

---
+++ app/src/main/java/cl/ucn/disc/dsm/dcanto/news/services/AppDatabase.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2021 David Canto-Alcayaga davidcanto01@gmail.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package cl.ucn.disc.dsm.dcanto.news.services;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
+
+
+import cl.ucn.disc.dsm.dcanto.news.model.News;
+import cl.ucn.disc.dsm.dcanto.news.model.NewsDao;
+import cl.ucn.disc.dsm.dcanto.news.utils.Converters;
+
+
+@Database(entities = {News.class}, version = 1)
+@TypeConverters(Converters.class)
+public abstract class AppDatabase extends RoomDatabase {
+
+    // Create Db Instance
+    private static AppDatabase localDb;
+
+    // Define database name
+    private static String DATABASE_NAME = "database";
+
+    /**
+     * Get's the instance of the database
+     *
+     * @param context to use
+     * @return localDB
+     */
+    public synchronized static AppDatabase getInstance(Context context) {
+
+        // Check condition
+        if (localDb == null) {
+
+            // If it's null, initialize
+            localDb = Room.databaseBuilder(context.getApplicationContext(),
+                    AppDatabase.class, DATABASE_NAME)
+                    .allowMainThreadQueries()
+                    .fallbackToDestructiveMigration()
+                    .build();
+
+        }
+
+        // Return instance
+        return localDb;
+    }
+
+    /**
+     * Creates the DAO instance
+     *
+     * @return
+     */
+    public abstract NewsDao newsDao();
+}
