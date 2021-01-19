fa4d9cc67a9726d923b87ac6675cf51b014593e8
tigerflight <david.canto@alumnos.ucn.cl>
2021-01-19 07:05:27 -0300

    feat(Model): add the classes to model the json response from the laravel api

---
+++ app/src/main/java/cl/ucn/disc/dsm/dcanto/news/model/newsjson/JsonNewsAttributes.java
@@ -0,0 +1,135 @@
+/*
+ * Copyright 2021 David Canto-Alcayaga davidcanto01@gmail.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package cl.ucn.disc.dsm.dcanto.news.model.newsjson;
+import java.util.Date.*;
+
+public class JsonNewsAttributes {
+
+  /**
+   * Unique ID.
+   */
+  private Long id;
+
+  /**
+   * The Title.
+   * Restrictions: not null, size > 2.
+   */
+  private String title;
+
+  /**
+   * The Author.
+   */
+  private String author;
+
+  /**
+   * The Source.
+   */
+  private String source;
+
+  /**
+   * The URL.
+   */
+  private String url;
+
+  /**
+   * The URL of image.
+   */
+  private String urlImage;
+
+  /**
+   * The Description.
+   */
+  private String description;
+
+  /**
+   * The Content.
+   */
+  private String content;
+
+  /**
+   * The Content.
+   */
+  private String publishedAt;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getAuthor() {
+    return author;
+  }
+
+  public void setAuthor(String author) {
+    this.author = author;
+  }
+
+  public String getSource() {
+    return source;
+  }
+
+  public void setSource(String source) {
+    this.source = source;
+  }
+
+  public String getUrl() {
+    return url;
+  }
+
+  public void setUrl(String url) {
+    this.url = url;
+  }
+
+  public String getUrlImage() {
+    return urlImage;
+  }
+
+  public void setUrlImage(String urlImage) {
+    this.urlImage = urlImage;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public String getContent() {
+    return content;
+  }
+
+  public void setContent(String content) {
+    this.content = content;
+  }
+
+  public String getPublishedAt() {
+    return publishedAt;
+  }
+
+  public void setPublishedAt(String publishedAt) {
+    this.publishedAt = publishedAt;
+  }
+
+
+}
