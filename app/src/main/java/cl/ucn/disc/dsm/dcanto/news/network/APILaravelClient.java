0c8ef8486b1f2c282663d2841c10f815f656b52b
tigerflight <david.canto@alumnos.ucn.cl>
2021-01-19 07:01:44 -0300

    feat(Network): add ApiLaravelClient to provide connection with the laravel server using the retrofit library and setting the base url for our server

---
+++ app/src/main/java/cl/ucn/disc/dsm/dcanto/news/network/APILaravelClient.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2021 David Canto-Alcayaga davidcanto01@gmail.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package cl.ucn.disc.dsm.dcanto.news.network;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class APILaravelClient {
+
+  private static Retrofit mRetrofit = null;
+
+  private static Retrofit getRetrofit(){
+    if(mRetrofit == null){
+      mRetrofit = new Retrofit.Builder()
+          .baseUrl("10.0.2.2:8000/api")
+          .addConverterFactory(GsonConverterFactory.create())
+          .build();
+    }
+
+    return mRetrofit;
+  }
+
+  public static APILaravelService getAPIService(){
+    return getRetrofit().create(APILaravelService.class);
+  }
+}
